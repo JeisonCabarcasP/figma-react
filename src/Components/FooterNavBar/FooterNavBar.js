8006bf511f815a21848307045be1f165efb05d94
Jeison Cabarcas <ancjeison@gmail.com>
2021-09-02 22:10:57 -0500

    maquetacion

---
+++ src/Components/FooterNavBar/FooterNavBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FooterNavBar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default FooterNavBar
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+        <div className="header-title">
+          <h3 class="title">Hola, Hannah</h3>
+        </div>
+        <div className="header-img">
+          <img src="/assets/icons/notifications_24px.svg" alt="Notificaciones" />
+        </div>
+      </header>
+    )
+}
+
+export default Header
