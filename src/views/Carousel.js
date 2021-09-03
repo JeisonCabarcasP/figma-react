41ca7fc5a687d6bacbd14834b9cb49010ed7a8e7
Jeison Cabarcas <ancjeison@gmail.com>
2021-09-02 22:56:22 -0500

    terminado

--- src/views/Carousel.js
+++ src/views/Carousel.js
@@ -1,27 +1,30 @@
 import React from 'react'
-
+import img1 from "../assets/img/thumb.png"
+import img2 from "../assets/img/Vector 6.png"
+import img3 from "../assets/icons/play_circle_outline_24px.svg"
+import icon from "../assets/icons/Counter.svg"
 const Carousel = () => {
     return (
         <div>
-                <section class="main-mjs">
-          <div class="main-ahorro-title">
-            <h2>Los P*t@s del ahorro</h2>
-        </div>
-      </section>
-      <section class="main-media">
-        <div class="main-media-container">
-          <img class="media-img" src="/assets/img/thumb.png" alt=""/>
-          <img class="media-backg" src="/assets/img/Vector 6.png" alt=""/>
-          <img class="media-icon" src="/assets/icons/play_circle_outline_24px.svg" alt=""/>
-          <h2>Esto es AHORRO</h2>
-          <p>Conoce como nuestro RoboAdvisor crea el mejor portafolio de inversión para ti.</p>
-        </div>
-      </section>
-      <section class="main-point">
-        <div class="main-point-container">
-          <img src="/assets/icons/Counter.svg" alt=""/>
-        </div>
-      </section>
+            <section class="main-mjs">
+                <div class="main-ahorro-title">
+                    <h2>Los P*t@s del ahorro</h2>
+                </div>
+            </section>
+            <section class="main-media">
+                <div class="main-media-container">
+                    <img class="media-img" src={img1} alt="" />
+                    <img class="media-backg" src={img2} alt="" />
+                    <img class="media-icon" src={img3} alt="" />
+                    <h2>Esto es AHORRO</h2>
+                    <p>Conoce como nuestro RoboAdvisor crea el mejor portafolio de inversión para ti.</p>
+                </div>
+            </section>
+            <section class="main-point">
+                <div class="main-point-container">
+                    <img src={icon} alt="" />
+                </div>
+            </section>
         </div>
     )
 }
--- src/views/Info.js
+++ src/views/Info.js
@@ -1,33 +1,35 @@
 import React from 'react'
-
+import img1 from "../assets/img/IMG1.png"
+import img2 from "../assets/img/IMG4.png"
+import img3 from "../assets/img/IMG5.png"
 const Info = () => {
     return (
         <div>
-                <section class="main-blog">
-        <div class="main-blog-container">
-         <div class="main-blog-frac1">
-            <div>
-             <h4>10 gastos que debes evitar</h4>
-             <p>Aunque el 66% de los adultos recibió educación para el ahorro, apenas el 44% ahorra de manera fomal.</p>
-            </div>
-            <img src="/assets/img/IMG1.png" alt=""/>
-         </div>
-         <div class="main-blog-frac2">
-          <div>
-            <h4>10 gastos que debes evitar</h4>
-            <p>Aunque el 66% de los adultos recibió educación para el ahorro, apenas el 44% ahorra de manera fomal.</p>
-          </div>
-          <img src="/assets/img/IMG4.png" alt=""/>
-        </div>
-        <div class="main-blog-frac3">
-          <div>
-            <h4>10 gastos que debes evitar</h4>
-            <p>Aunque el 66% de los adultos recibió educación para el ahorro, apenas el 44% ahorra de manera fomal.</p>
-          </div>
-          <img src="/assets/img/IMG5.png" alt=""/>
-        </div>
-        </div>
-      </section>
+            <section class="main-blog">
+                <div class="main-blog-container">
+                    <div class="main-blog-frac1">
+                        <div>
+                            <h4>10 gastos que debes evitar</h4>
+                            <p>Aunque el 66% de los adultos recibió educación para el ahorro, apenas el 44% ahorra de manera fomal.</p>
+                        </div>
+                        <img src={img1} alt="" />
+                    </div>
+                    <div class="main-blog-frac2">
+                        <div>
+                            <h4>10 gastos que debes evitar</h4>
+                            <p>Aunque el 66% de los adultos recibió educación para el ahorro, apenas el 44% ahorra de manera fomal.</p>
+                        </div>
+                        <img src={img2} alt="" />
+                    </div>
+                    <div class="main-blog-frac3">
+                        <div>
+                            <h4>10 gastos que debes evitar</h4>
+                            <p>Aunque el 66% de los adultos recibió educación para el ahorro, apenas el 44% ahorra de manera fomal.</p>
+                        </div>
+                        <img src={img3} alt="" />
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
